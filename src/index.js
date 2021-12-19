95b7a8183359678ef30941737536e4e27f9ddef1
MeFaltaUnVerano <trolleros200@gmail.com>
2021-12-19 17:48:05 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import Paddle from "./paddle";
+import InputHandler from "./input";
+import Ball from "./ball";
+let canvas = document.getElementById("gameScreen");
+
+let ctx = canvas.getContext("2d");
+
+const GAME_WIDTH = 800;
+const GAME_HEIGHT = 600;
+
+let paddle = new Paddle(GAME_WIDTH, GAME_HEIGHT);
+let ball = new Ball();
+new InputHandler(paddle);
+
+let lastTime = 0;
+
+function gameLoop(timestamp) {
+  let deltaTime = timestamp - lastTime;
+  lastTime = timestamp;
+
+  ctx.clearRect(0, 0, GAME_WIDTH, GAME_HEIGHT);
+
+  paddle.update(deltaTime);
+  paddle.draw(ctx);
+  ball.draw(ctx);
+  requestAnimationFrame(gameLoop);
+}
+
+requestAnimationFrame(gameLoop);
